8244ec1ef02a368a866c732740bd28e8ab96bbf4
Tuan-solit <dangngoctuan96nt@gmail.com>
2024-04-17 20:55:31 +0700

    part 1

---
+++ client/src/pages/AboutUs/AboutUs.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div className="flex-grow-0 pt-64 lg:flex-grow-1 md:block">
+      <div className="relative overflow-hidden bg-center bg-cover top md:block md:pt-80 md:pb-80">
+        <div className="box-border w-full px-24 mx-auto sm:text-lg">
+          <div className="text-center md:block">
+            <div className="inline-flex font-sans text-3xl font-extrabold leading-5 text-green-600 font-public-sans sm:text-lg">
+              <span className="opacity-100 transform-none">W</span>
+              <span className="opacity-100 transform-none">h</span>
+              <span className="opacity-100 transform-none">o</span>
+            </div>
+            <br />
+            <div className="flex inline-flex flex-row gap-4 text-white">
+              <div className="inline-flex font-sans text-3xl font-extrabold leading-5 font-public-sans">
+                <span className="opacity-100 transform-none">w</span>
+                <span className="opacity-100 transform-none">e</span>
+                <span className="opacity-100 transform-none">a</span>
+                <span className="opacity-100 transform-none">r</span>
+                <span className="opacity-100 transform-none">e</span>
+                <span className="opacity-100 transform-none">?</span>
+              </div>
+            </div>
+            <div className="opacity-100 transform-none">
+              <h4 className="mt-24 mb-0 text-base font-semibold leading-6 text-white font-public-sans sm:text-lg">
+                "Let's work together and" <br />
+                "Make awesome site easily"
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
