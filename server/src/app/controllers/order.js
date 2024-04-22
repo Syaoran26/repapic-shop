9388e29a6af5885feb85dcccd809b1b7a691cf6c
Syaoran Nguyen <jinnguyen054@gmail.com>
2024-04-22 23:31:52 +0700

    Eslint Config

--- server/src/app/controllers/order.js
+++ server/src/app/controllers/order.js
@@ -1,58 +1,38 @@
 import Order from '../models/Order.js';
 import asyncHandler from 'express-async-handler';
-import { createError } from '../../utils/error.js';
+import { ErrorWithStatus } from '../../utils/error.js';
 
 export const createOrder = asyncHandler(async (req, res) => {
   const newOrder = new Order(req.body);
-  try {
-    const order = await newOrder.save();
-    res.status(201).json(order);
-  } catch (error) {
-    throw error;
-  }
+  const order = await newOrder.save();
+  res.status(201).json(order);
 });
 
 export const updateOrder = asyncHandler(async (req, res) => {
-  try {
-    const order = await Order.findOneAndUpdate({ _id: req.params.id }, { $set: req.body }, { new: true });
-    if (!order) {
-      throw createError(404, 'Không tìm thấy Order!');
-    }
-    res.status(200).json(order);
-  } catch (error) {
-    throw error;
+  const order = await Order.findOneAndUpdate({ _id: req.params.id }, { $set: req.body }, { new: true });
+  if (!order) {
+    throw new ErrorWithStatus(404, 'Không tìm thấy Order!');
   }
+  res.status(200).json(order);
 });
 
 export const deleteOrder = asyncHandler(async (req, res) => {
-  try {
-    const order = await Order.findOneAndDelete({ _id: req.params.id });
-    if (!order) {
-      throw createError(404, 'Không tìm thấy Order!');
-    }
-    res.status(200).json(order);
-  } catch (error) {
-    throw error;
+  const order = await Order.findOneAndDelete({ _id: req.params.id });
+  if (!order) {
+    throw new ErrorWithStatus(404, 'Không tìm thấy Order!');
   }
+  res.status(200).json(order);
 });
 
 export const getOrder = asyncHandler(async (req, res) => {
-  try {
-    const order = await Order.findOne({ _id: req.params.id });
-    if (!order) {
-      throw createError(404, 'Không tìm thấy Order!');
-    }
-    res.status(200).json(order);
-  } catch (error) {
-    throw error;
+  const order = await Order.findOne({ _id: req.params.id });
+  if (!order) {
+    throw new ErrorWithStatus(404, 'Không tìm thấy Order!');
   }
+  res.status(200).json(order);
 });
 
 export const getOrders = asyncHandler(async (req, res) => {
-  try {
-    const orders = await Order.find();
-    res.status(200).json(orders);
-  } catch (error) {
-    throw error;
-  }
+  const orders = await Order.find();
+  res.status(200).json(orders);
 });
