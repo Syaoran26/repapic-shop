66faf6f0e698a08712df250d23183e03f7e7002d
Syaoran Nguyen <jinnguyen054@gmail.com>
2024-04-25 22:37:58 +0700

    Services

--- client/src/pages/Shop/ProductDetail/ProductDetail.tsx
+++ client/src/pages/Shop/ProductDetail/ProductDetail.tsx
@@ -1,14 +1,12 @@
-import { useState } from 'react';
-import { Breadcrumbs, Button, Chip, Container, Divider, Link, Rating, Stack } from '@mui/material';
-import ProductSlide from './ProductSlide';
-import Quantity from '~/components/Quantity';
+import { Breadcrumbs, Container, Link } from '@mui/material';
 import config from '~/config';
-import { formatPrice } from '~/utils/format';
-import { AddToCartIcon } from '~/components/Icons';
+import ProductSlide from './ProductSlide';
+import ProductMain from './ProductMain';
+import { IoCard, IoGift } from 'react-icons/io5';
+import { FaHeadphones } from 'react-icons/fa6';
+import { BiSolidDiscount } from 'react-icons/bi';
 
 const ProductDetail = () => {
-  const [quantity, setQuantity] = useState(3);
-
   return (
     <div className="pt-16 md:pt-20">
       <Container className="mt-10">
@@ -24,40 +22,34 @@ const ProductDetail = () => {
             <span>Tên sản phẩm</span>
           </Breadcrumbs>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12">
-          <div className="col-span-1 p-3 lg:col-span-7 lg:p-5 xl:p-8">
+        <div className="grid grid-cols-1 gap-10 lg:grid-cols-2 xl:grid-cols-12 xl:gap-16">
+          <div className="col-span-1 xl:col-span-7">
             <ProductSlide />
           </div>
-          <div className="col-span-1 p-3 lg:col-span-5 lg:p-5 xl:p-8">
-            <Stack gap={2}>
-              <div>
-                <Chip label="MỚI" color="info" size="small" />
-              </div>
-              <h5 className="text-xl font-bold">Foundations Matte Flip Flop</h5>
-              <Stack direction="row" alignItems="center">
-                <Rating readOnly value={4.5} precision={0.25} />
-                <span className="text-sm text-fade">(20 đánh giá)</span>
-              </Stack>
-              <span className="text-xl font-bold">{formatPrice(120000)}</span>
-              <p className="text-sm text-fade">
-                Với thiết kế gợn sóng nguyên bản lấy cảm hứng từ tàu cao tốc Nhật Bản, Nike Air Max 97 cho phép bạn đẩy
-                mạnh phong cách của mình về phía trước.
-              </p>
-              <Divider style={{ borderStyle: 'dashed' }} />
-              <Stack direction="row">
-                <span className="flex-1 text-sm font-semibold">Số lượng</span>
-                <Quantity value={quantity} available={74} onChange={setQuantity} />
-              </Stack>
-              <Divider style={{ borderStyle: 'dashed' }} />
-              <Stack direction="row" gap={2}>
-                <Button size="large" fullWidth color="primary" startIcon={<AddToCartIcon />}>
-                  Thêm vào giỏ hàng
-                </Button>
-                <Button size="large" fullWidth>
-                  Mua ngay
-                </Button>
-              </Stack>
-            </Stack>
+          <div className="col-span-1 xl:col-span-5">
+            <ProductMain />
+          </div>
+        </div>
+        <div className="grid gap-10 my-20 md:grid-cols-2 lg:grid-cols-4">
+          <div className="px-8 text-center">
+            <FaHeadphones className="inline-block text-primary" size={32} />
+            <h6 className="mt-2 mb-1 font-semibold">Hỗ trợ 24/7</h6>
+            <p className="text-sm text-fade">Mua sắm với những sự tư vấn của chúng tôi</p>
+          </div>
+          <div className="px-8 text-center">
+            <IoGift className="inline-block text-primary" size={32} />
+            <h6 className="mt-2 mb-1 font-semibold">Ưu đãi bất ngờ hàng ngày</h6>
+            <p className="text-sm text-fade">Có cơ hội nhận được ưu đãi và tiết kiệm tới 20%</p>
+          </div>
+          <div className="px-8 text-center">
+            <BiSolidDiscount className="inline-block text-primary" size={32} />
+            <h6 className="mt-2 mb-1 font-semibold">Giá cả phải chăng</h6>
+            <p className="text-sm text-fade">Sản phẩm được làm thủ công trực tiếp tại xưởng</p>
+          </div>
+          <div className="px-8 text-center">
+            <IoCard className="inline-block text-primary" size={32} />
+            <h6 className="mt-2 mb-1 font-semibold">Thanh toán bảo mật</h6>
+            <p className="text-sm text-fade">Yên tâm thanh toán một cách tiện lợi</p>
           </div>
         </div>
       </Container>
