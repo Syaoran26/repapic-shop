5d165b44823b96cc73a01141e1e07eb37e0202a6
TuanKiet <kietntde160480@fpt.edu.com>
2024-05-06 23:08:30 +0700

    wishlist

--- server/src/app/controllers/user.js
+++ server/src/app/controllers/user.js
@@ -2,6 +2,10 @@ import User from '../models/User.js';
 import asyncHandler from 'express-async-handler';
 import { ErrorWithStatus } from '../../utils/error.js';
 import { getAddress } from '../../utils/address.js';
+import Product from '../models/Product.js';
+import jwt from 'jsonwebtoken'
+import {promisify} from 'util'
+import mongoose from 'mongoose';
 
 export const createUser = asyncHandler(async (req, res) => {
   const { mimetype, buffer } = req.file;
@@ -49,3 +53,75 @@ export const getUsers = asyncHandler(async (req, res) => {
 
   res.status(200).json(sanitizedUsers);
 });
+
+export const getWishList = asyncHandler(async (req, res) => {
+  let token;
+  if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+    token = req.headers.authorization.split(' ')[1];
+  }
+  const user = await promisify(jwt.verify)(token, process.env.ACCESS_TOKEN);
+  let userWishList = await User.findById(user.id).populate('wishlist')
+  if(!userWishList.wishlist){
+    throw new ErrorWithStatus('Danh sách Wishlist rỗng')
+  }
+  res.status(200).json(userWishList.wishlist)
+  
+});
+
+export const addProductToWishList = asyncHandler(async (req,res)=>{
+  let token;
+  if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+    token = req.headers.authorization.split(' ')[1];
+  }
+  const user = await promisify(jwt.verify)(token, process.env.ACCESS_TOKEN);
+  const productIdList = req.body;
+  let validProduct = [];
+  let nonValidProduct = [];
+
+  let promises = productIdList.map(async(productId)=>{
+    let product = await Product.findById(new mongoose.Types.ObjectId(productId)).populate('category');
+    let userWishList = await User.findById(user.id).populate('wishlist');
+    let productWishList = userWishList.wishlist.find(item => item._id.toString() === productId);
+
+    if(product && !productWishList){
+      validProduct.push(productId)
+    }else{
+      nonValidProduct.push(productId)
+    }
+  })
+
+  await Promise.all(promises);
+  
+  if(validProduct.length === 0){
+    throw new ErrorWithStatus(501, 'Product Không tồn tại')
+  }
+    const updateUser = await User.findByIdAndUpdate(user.id,
+      {$push:{wishlist:{$each:validProduct}}},
+      {new : true}
+    )
+    res.status(200).json({
+      ProductAddSuccess: updateUser,
+      ProductNotFound: nonValidProduct
+    })
+})
+
+export const deleteProductToWishList = asyncHandler(async(req, res)=>{
+  let productIdList = req.body
+  
+  let token;
+  if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+    token = req.headers.authorization.split(' ')[1];
+  }
+  const user = await promisify(jwt.verify)(token, process.env.ACCESS_TOKEN);
+  let userWishList = await User.findById(user.id).populate('wishlist')
+  if(!userWishList.wishlist){
+    throw new ErrorWithStatus('Danh sách Wishlist rỗng')
+  }
+  let productWishList = userWishList.wishlist.filter(item => !productIdList.includes(item._id.toString()))
+  userWishList.wishlist=[...productWishList]
+
+  userWishList.save()
+  
+  res.status(200).json({status:"Deleted Success"})
+
+})
--- server/src/routes/me.js
+++ server/src/routes/me.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { getUser, getCart, changePassword } from '../app/controllers/me.js';
+import { getWishList, addProductToWishList, deleteProductToWishList } from '../app/controllers/user.js';
 
 const router = Router();
 
 router.get('/cart', getCart);
 router.get('/', getUser);
 router.patch('/change-password', changePassword);
-
+router.get('/wishlist', getWishList);
+router.post('/wishlist', addProductToWishList)
+router.delete('/wishlist', deleteProductToWishList)
 export default router;
