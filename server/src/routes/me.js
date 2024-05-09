d44ae39c2d6075a1d449af6fa8739af6825daf31
Tuan Kiet <132985771+TuanKiet55@users.noreply.github.com>
2024-05-09 22:11:39 +0700

    Wishlist API

--- server/src/routes/me.js
+++ server/src/routes/me.js
@@ -1,10 +1,20 @@
 import { Router } from 'express';
-import { getUser, getCart, changePassword } from '../app/controllers/me.js';
+import {
+  getUser,
+  getCart,
+  changePassword,
+  getWishList,
+  addProductToWishList,
+  deleteProductToWishList,
+} from '../app/controllers/me.js';
+import {} from '../app/controllers/user.js';
 
 const router = Router();
 
 router.get('/cart', getCart);
 router.get('/', getUser);
 router.patch('/change-password', changePassword);
-
+router.get('/wishlist', getWishList);
+router.post('/wishlist/:productId', addProductToWishList);
+router.patch('/wishlist/:productId', deleteProductToWishList);
 export default router;
