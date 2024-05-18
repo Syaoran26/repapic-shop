2282e02f2b0d2661050f2c9f1b9e7fd072322dfb
Syaoran Nguyen <jinnguyen054@gmail.com>
2024-05-19 01:10:48 +0700

    Order Logic optimize

--- client/src/pages/Cart/constants.ts
+++ client/src/pages/Cart/constants.ts
@@ -1,6 +1,16 @@
-export const paymentEnum = {
-  PayOS: 1,
-  Cash: 2,
+export enum PaymentEnum {
+  PayOS = 1,
+  Cash = 2,
+}
+
+export enum ShippingEnum {
+  Standard = 1,
+  Express = 2,
+}
+
+export const ShippingCost: { [key in ShippingEnum]: number } = {
+  [ShippingEnum.Standard]: 15000,
+  [ShippingEnum.Express]: 30000,
 };
 
 export const fakePaymentLink = 'https://pay.payos.vn/web/14b040a6294a4099bf80493bf3d44015';
